7d202252480ee707fc648b257be4d2a77df5e5bc
Hack Reactor Student <student@hackreactor.com>
2017-11-03 18:48:46 -0700

    Setup database and tables

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,32 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  objectId INT PRIMARY KEY AUTO_INCREMENT,
+  userId INT,
+  text TEXT,
+  room TEXT,
+  createdAt DATETIME,
+  updatedAt DATETIME
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username TEXT,
+  textColor TEXT,
+  fontFamily TEXT,
+  signOffMsg TEXT,
+  lastSeen DATETIME
+);
 
-
-
+CREATE TABLE friends (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  userId INT,
+  friendId INT,
+  friendsFrom DATETIME,
+  FOREIGN KEY (userId, friendId)
+    REFERENCES users(id, id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
